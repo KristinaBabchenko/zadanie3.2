93496f69e8a50b06429b53ed05bd40831244eafc
abuzik999 <kdy11@mail.ru>
2023-05-04 11:16:22 +0500

    neww commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int account, bonus, replenish;
 
-        account = 100;
-        replenish = 1000;
-        bonus = replenish / 100;
+        int account = 100;
+        int replenish = 1000;
+        int bonus = replenish / 100;
 
         if (replenish >= 1000) {
             account = replenish + account + bonus;
